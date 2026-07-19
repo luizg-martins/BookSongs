116c49a9f22199f4d46a7275e5e75be9e4899282
Luiz Gustavo <l.h.devamant@gmail.com>
2026-07-19 06:14:11 -0300

    Início do bookSongs
    Feita as 3 tabelas: livro, musica e associacao, e adicionado o primeiro dado das três

---
+++ glossario.md
@@ -0,0 +1,13 @@
+# Glossário SQL
+## -- 
+Comentário
+## INTEGER 
+Número Inteiro (Int)
+## TEXT 
+Texto (String)
+## PRIMARY KEY 
+Chave que não se repete, usada para identificar uma entidade
+## AUTOINCREMENT 
+O SQLite gera o dado automaticamente
+## FOREIGN KEY
+ Usa uma chave primária de outro atributo para atribuir um ao outro
---
+++ inserts.sql
@@ -0,0 +1,12 @@
+INSERT INTO livro (nome, autor, genero, saga, paginas, cover) VALUES 
+('Condão', 'Giordano Mochel Netto', 'Ficção Científica', NULL, 400, 'E:\L1\js\booksSongs\bookCovers\condao.jpg'),
+
+INSERT INTO musica (nome, artista, estilo, album, duracao, cover) VALUES 
+('It Came from the TV', 'Secession Studios', 'Orquestra/Trilha Sonora', 'Vae Victis', 203, 'E:\L1\js\booksSongs\albumCovers\vaeVictis.jpg'),
+
+INSERT INTO associacao (id_livro, id_musica, nivel_associacao, motivo_associacao) VALUES 
+(1, 1, 7, 'Apesar de não lembrar de nenhum momento específico da leitura, o pacing da música ser
+calmo e com poucas elevações me lembra de determinados momentos de calmaria na jornada dos personagens, com as batidas
+contínuas e o ritmo constante me lembrando de cenas com mais níveis de ação e descobertas dos personagens. Além disso, o final
+contrasta maravilhosamente bem com o final apoteótico, mas taciturno e ao mesmo tempo levemente inquietante
+da história, e de seu protagonista e antagonista.'),
